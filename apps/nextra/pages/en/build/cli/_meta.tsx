81bb7ec523dfd06d871dee7d6e1d10388f4b2bac
ADNANALISALHADABY <139702240+ADNANALISALHADABY@users.noreply.github.com>
2024-12-23 06:41:09 +0300

    Initial commit

---
+++ apps/nextra/pages/en/build/cli/_meta.tsx
@@ -0,0 +1,40 @@
+export default {
+  "---get-started---": {
+    type: "separator",
+    title: "Get Started",
+  },
+  "install-cli": {
+    title: "Installation",
+  },
+  "setup-cli": {
+    title: "Setup",
+  },
+  "start-from-template": {
+    title: "Start from template",
+  },
+  "---usage---": {
+    type: "separator",
+    title: "Usage",
+  },
+  "working-with-move-contracts": {
+    title: "Working With Move Contracts",
+  },
+  "formatting-move-contracts": {
+    title: "Formatting Move Contracts",
+  },
+  "trying-things-on-chain": {
+    title: "Trying Things On-Chain",
+  },
+  "replay-past-transactions": {
+    title: "Replaying Past Transactions",
+  },
+  "running-a-local-network": {
+    title: "Running a Local Network",
+  },
+  "managing-a-network-node": {
+    title: "Managing a Network Node",
+  },
+  "public-network": {
+    title: "Running a Public Network (Advanced)",
+  },
+};
