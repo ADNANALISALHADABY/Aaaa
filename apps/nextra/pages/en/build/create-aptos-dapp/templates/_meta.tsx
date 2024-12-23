e7f735a1dc919a63bdfbc8a5d2767d2dd62ea34d
ADNANALISALHADABY <139702240+ADNANALISALHADABY@users.noreply.github.com>
2024-12-23 06:41:33 +0300

    Initialize update_cad_gif

---
+++ apps/nextra/pages/en/build/create-aptos-dapp/templates/_meta.tsx
@@ -0,0 +1,17 @@
+export default {
+  boilerplate: {
+    title: "Boilerplate Template",
+  },
+  "clicker-game-tg-mini-app": {
+    title: "Telegram Clicker Game Mini App Template",
+  },
+  "nft-minting-dapp": {
+    title: "NFT minting dapp Template",
+  },
+  "token-minting-dapp": {
+    title: "Token minting dapp Template",
+  },
+  "token-staking-dapp": {
+    title: "Token staking dapp Template",
+  },
+};
