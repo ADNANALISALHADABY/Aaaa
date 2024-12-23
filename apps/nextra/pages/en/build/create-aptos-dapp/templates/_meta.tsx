8e7a17613bb0029cb486e60dfa47bac0bd93a01d
ADNANALISALHADABY <139702240+ADNANALISALHADABY@users.noreply.github.com>
2024-12-23 06:41:43 +0300

    Initialize j/update-indexer-template

---
+++ apps/nextra/pages/en/build/create-aptos-dapp/templates/_meta.tsx
@@ -0,0 +1,20 @@
+export default {
+  boilerplate: {
+    title: "Boilerplate Template",
+  },
+  "clicker-game-tg-mini-app": {
+    title: "Telegram Clicker Game Mini App Template",
+  },
+  "nft-minting-dapp": {
+    title: "NFT minting dapp Template",
+  },
+  "token-minting-dapp": {
+    title: "Token minting dapp Template",
+  },
+  "token-staking-dapp": {
+    title: "Token staking dapp Template",
+  },
+  "custom-indexer": {
+    title: "Custom Indexer Template",
+  },
+};
---
+++ apps/nextra/pages/en/build/sdks/community-sdks/kotlin-sdk/_meta.tsx
@@ -0,0 +1,28 @@
+export default {
+  "---get-started---": {
+    type: "separator",
+    title: "Get Started",
+  },
+  quickstart: {
+    title: "Quickstart",
+  },
+  "---usage---": {
+    type: "separator",
+    title: "Usage",
+  },
+  account: {
+    title: "Creating and Managing Accounts",
+  },
+  "fetch-data-via-sdk": {
+    title: "Fetch Data via the Kotlin SDK",
+  },
+  "building-transactions": {
+    title: "Building and Sending Transactions",
+  },
+  "for-ios-devs": {
+    title: "For iOS Developers",
+  },
+  "client-configuration": {
+    title: "Client Configuration",
+  },
+};
