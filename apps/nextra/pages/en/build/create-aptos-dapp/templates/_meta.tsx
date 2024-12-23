304c51175fa6b042d7d0f554e60f921af4ca1a29
ADNANALISALHADABY <139702240+ADNANALISALHADABY@users.noreply.github.com>
2024-12-23 06:41:32 +0300

    Initialize j/update-cad-boilerplate-template

---
+++ apps/nextra/pages/en/build/create-aptos-dapp/templates/_meta.tsx
@@ -0,0 +1,14 @@
+export default {
+  boilerplate: {
+    title: "Boilerplate Template",
+  },
+  "nft-minting-dapp": {
+    title: "NFT minting dapp Template",
+  },
+  "token-minting-dapp": {
+    title: "Token minting dapp Template",
+  },
+  "token-staking-dapp": {
+    title: "Token staking dapp Template",
+  },
+};
---
+++ apps/nextra/pages/en/build/sdks/_meta.tsx
@@ -0,0 +1,29 @@
+export default {
+  "ts-sdk": {
+    title: "TypeScript SDK",
+  },
+  "python-sdk": {
+    title: "Python SDK",
+  },
+  "rust-sdk": {
+    title: "Rust SDK",
+  },
+  "unity-sdk": {
+    title: "Unity SDK",
+  },
+  "go-sdk": {
+    title: "Go SDK",
+  },
+  "cpp-sdk": {
+    title: "C++ / Unreal SDK",
+  },
+  "swift-sdk": {
+    title: "Swift SDK",
+  },
+  "kotlin-sdk": {
+    title: "Kotlin SDK",
+  },
+  "wallet-adapter": {
+    title: "Wallet Adapter",
+  },
+};
