71a8af3d0c0d024cf32d650bce7fad82eb811d51
ADNANALISALHADABY <139702240+ADNANALISALHADABY@users.noreply.github.com>
2024-12-23 06:41:44 +0300

    Initialize 1112-alden-storage-doc

---
+++ apps/nextra/pages/en/build/create-aptos-dapp/_meta.tsx
@@ -0,0 +1,10 @@
+export default {
+  templates: {
+    title: "Templates",
+    href: "https://learn.aptoslabs.com/en/dapp-templates",
+    newWindow: true,
+  },
+  faq: {
+    title: "FAQ",
+  },
+};
---
+++ apps/nextra/pages/zh/build/_meta.tsx
@@ -0,0 +1,5 @@
+export default {
+  "get-started": "開始",
+  cli: "CLI",
+  apis: "APIs",
+};
